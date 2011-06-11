4db0cd054a5b346a4517b2beee621d71c3ab574c
Marcus Ilgner <mail@marcusilgner.com>
2011-06-11 17:49:54 +0200

    basic functionality working: issue creation & occurrence counting moved formatting into view templates improved readme

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 # Hoptoad Server v2 for Redmine
 
-An implementation of the Hoptoad protocol for Redmine which allows you to receive bugs within Redmine.
-Inspired by and loosely based on https://github.com/yeah/redmine_hoptoad_server
+An implementation of the Hoptoad protocol for Redmine which automatically creates issues from notices submitted via Hoptoad notifiers.
+Inspired by and loosely based on the [v1 Hoptoad server by Jan Schulz-Hofen](https://github.com/yeah/redmine_hoptoad_server)
+
+
+## Installation
+
+Install the hpricot gem, then install the plugin into Redmine [as usual](http://www.redmine.org/projects/redmine/wiki/Plugins).
+Now go to _Administration -> Settings -> Incoming emails_ and, if neccessary, check the box _Enable WS for incoming emails_ and generate and API key. This is the key you will need in the next step to configure your notifier.
+
+
+## Client-Configuration
+
+For a rails application, just setup the Hoptoad notifier as usual, then modify `config/initializers/hoptoad.rb` according to your using this template:
+
+	HoptoadNotifier.configure do |config|
+	  config.api_key = {:project => 'redmine_project_identifier',    # the identifier you specified for your project in Redmine
+	                    :tracker => 'Bug',                           # the name of your Tracker of choice in Redmine
+	                    :api_key => 'my_redmine_api_key',            # the key you generated before in Redmine (NOT YOUR HOPTOAD API KEY!)
+	                    :category => 'Development',                  # the name of a ticket category (optional.)
+	                    :assigned_to => 'admin',                     # the login of a user the ticket should get assigned to by default (optional.)
+	                    :priority => 5                               # the default priority (use a number, not a name. optional.)
+	                   }.to_yaml
+	  config.host = 'my_redmine_host.com'                            # the hostname your Redmine runs at
+	  config.port = 443                                              # the port your Redmine runs at
+	  config.secure = true                                           # sends data to your server via SSL (optional.)
+	end
+
+That's it. You may `rake hoptoad:test` to generate a test issue.
+If it doesn't work, check your logs and configuration, then [submit an issue on Github](https://github.com/milgner/redmine_hoptoad_server_v2/issues)
+
+
+## License
+
+This plugin is licensed under the [Apache license 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+
+## Author
+
+Written by [Marcus Ilgner](mailto:mail@marcusilgner.com)
--- app/controllers/hoptoad_controller.rb
+++ app/controllers/hoptoad_controller.rb
@@ -1,87 +1,133 @@
+require 'hpricot'
+
 class HoptoadController < ApplicationController
+  before_filter :find_or_create_custom_fields
+  
   unloadable
   
   def index
-    redirect_to '/' unless request.post? && !params[:notice].empty?
-    notice = params[:notice]
-    redmine_params = YAML.load(notice['api_key'])
-    raise ArgumentError.new("invalid API key") unless Setting.mail_handler_api_key == redmine_params[:api_key]
+    redirect_to root_path unless request.post? && !params[:notice].empty?
+    @notice = params[:notice]
+    if (@notice['version'] != "2.0")
+      logger.warn("Expected Hoptoad notice version 2.0 but got #{@notice['version']}. You should consider filing an enhancement request or updating the plugin.")
+    end
 
-    settings = read_settings(redmine_params)
-
-    subject = build_subject(notice)    
-    issue = Issue.find_by_subject_and_project_id_and_tracker_id(subject, settings[:project].id, settings[:tracker].id)
+    restore_var_elements(request.body)
     
-    if issue.nil?
-      create_new_issue(notice, settings)
+    redmine_params = YAML.load(@notice['api_key'])
+    raise ArgumentError.new("Invalid API key") unless Setting.mail_handler_api_key == redmine_params[:api_key]
+
+    read_settings(redmine_params)
+
+    subject = build_subject
+    @issue = Issue.find_by_subject_and_project_id_and_tracker_id(subject, @settings[:project].id, @settings[:tracker].id)
+    
+    if @issue.nil?
+      create_new_issue
     else
-      update_existing_issue(issue, notice)
+      update_existing_issue
     end
     
-    render :text => 'OK'
+    render :layout => false
   end
   
   private
 
+  # The automagic XML parsing by Rails ignores the text elements
+  # This method replaces the garbled elements by new arrays of hashes
+  def restore_var_elements(original_xml)
+    doc = Hpricot::XML(request.body)
+    
+    request_params = convert_var_elements(doc/'/notice/request/params/var')
+    request_params.delete('action') # already known
+    request_params.delete('controller') # already known    
+    @notice['request']['params'] = request_params
+    
+    cgi_data = convert_var_elements(doc/'notice/request/cgi-data/var')
+    @notice['request']['cgi_data'] = cgi_data
+    
+    session_vars = convert_var_elements(doc/'/notice/request/session/var')
+    @notice['request']['session'] = session_vars
+  end
+  
+  def convert_var_elements(elements)
+    result = {}
+    elements.each do |elem|
+      result[elem.attributes['key']] = elem.inner_text
+    end
+    result
+  end
+  
   def read_settings(params)
-    settings = {}
-    settings[:project] = Project.find_by_identifier(params[:project]) or raise ArgumentError.new("invalid project #{params[:project]}")
-    settings[:tracker] = settings[:project].trackers.find_by_name(params[:tracker]) or raise ArgumentError.new("tracker #{params[:tracker]} not found in project #{params[:project]}")
-
+    @settings = {}
+    @settings[:project] = Project.find_by_identifier(params[:project]) or raise ArgumentError.new("invalid project #{params[:project]}")
+    @settings[:tracker] = @settings[:project].trackers.find_by_name(params[:tracker]) or raise ArgumentError.new("tracker #{params[:tracker]} not found in project #{params[:project]}")
+    check_custom_field_assignments
     # these are optional
-    settings[:category] = IssueCategory.find_by_name(params[:category]) unless params[:category].blank?
-    settings[:assigned_to] = User.find_by_login(params[:assigned_to]) unless params[:assigned_to].blank?
-    settings[:priority] = params[:priority] unless params[:priority].blank?
-    settings
+    @settings[:category] = IssueCategory.find_by_name(params[:category]) unless params[:category].blank?
+    @settings[:assigned_to] = User.find_by_login(params[:assigned_to]) unless params[:assigned_to].blank?
+    @settings[:priority] = params[:priority] unless params[:priority].blank?
   end
   
-  def create_new_issue(notice, settings)
-    issue = Issue.new
-    issue.author = User.anonymous
-    issue.subject = build_subject(notice)
-    issue.tracker = settings[:tracker]
-    issue.project = settings[:project]
-    issue.category = settings[:category]
-    issue.assigned_to = settings[:assigned_to]
-    issue.priority_id = settings[:priority]
-    issue.description = build_description(notice, settings)
-    issue.status = issue_status_open
-    issue.save!
+  def create_new_issue
+    @issue = Issue.new
+    @issue.author = User.anonymous
+    @issue.subject = build_subject
+    @issue.tracker = @settings[:tracker]
+    @issue.project = @settings[:project]
+    @issue.category = @settings[:category]
+    @issue.assigned_to = @settings[:assigned_to]
+    @issue.priority_id = @settings[:priority]
+    @issue.description = render_to_string(:partial => 'issue_description')
+    @issue.status = issue_status_open
+    @issue.custom_values.build(:custom_field => @occurrences_field, :value => '1')
+    @issue.custom_values.build(:custom_field => @environment_field, :value => @notice['server_environment']['environment_name'])
+    @issue.custom_values.build(:custom_field => @version_field, :value => @notice['server_environment']['app_version']) unless @notice['server_environment']['app_version'].nil?
+    @issue.save!
   end
   
-  def update_existing_issue(issue, notice)
-    issue.status = issue_status_open
+  def check_custom_field_assignments
+    [@occurrences_field, @environment_field, @version_field].each do |field|
+      @settings[:project].issue_custom_fields << field unless @settings[:project].issue_custom_fields.include?(field)
+      @settings[:tracker].custom_fields << field unless @settings[:tracker].custom_fields.include?(field)
+    end
+  end
+  
+  def update_existing_issue
+    @issue.status = issue_status_open if @issue.status.is_closed?
+    number_occurrences = @issue.custom_value_for(@occurrences_field.id).value
+    @issue.custom_field_values = { @occurrences_field.id => (number_occurrences.to_i+1).to_s }
+    @issue.save!
   end
   
   def issue_status_open
     IssueStatus.find(:first, :conditions => {:is_default => true}, :order => 'position ASC')
   end
+   
+  def build_subject
+    error_class = @notice['error']['class']
+    file = @notice['error']['backtrace']['line'].first()['file']
+    line = @notice['error']['backtrace']['line'].first()['number']
+    "[Hoptoad] #{error_class} in #{file}:#{line}"
+  end
   
-  def build_description(notice, settings)
-    error = "The Hoptoad notifier reported an error: <pre>#{notice['error']['message']}</pre>\n\n"
-    error << "Backtrace:\n\np((. "
-    notice['error']['backtrace']['line'].each do |element|
-      if (settings[:project].repository && element['file'].start_with?('[PROJECT_ROOT]') )
-        error << "source:\"#{element['file'][14..-1]}#L#{element['number']}\" in ??<notextile>#{element['method']}</notextile>??"
-      else
-        error << "@#{element['file']}:#{element['number']}@ in ??<notextile>#{element['method']}</notextile>??"
-      end
-      error << "\r\n"
+  def find_or_create_custom_fields
+    @occurrences_field = IssueCustomField.find_or_initialize_by_name('# Occurrences')
+    if @occurrences_field.new_record?
+      @occurrences_field.attributes = {:field_format => 'int', :default_value => '1', :is_filter => true}
+      @occurrences_field.save(false)
     end
-    error
-  end
-  
-  def short_error_info(notice)
-    info = {}
-    # logger.debug("Notice backtrace: #{notice['error']['backtrace'].inspect}")
-    info[:error_class] = notice['error']['class']
-    info[:file] = notice['error']['backtrace']['line'].first()['file']
-    info[:line] = notice['error']['backtrace']['line'].first()['number']
-    info
-  end
-  
-  def build_subject(notice)
-    error_info = short_error_info(notice)
-    "[Hoptoad] #{error_info[:error_class]} in #{error_info[:file]}:#{error_info[:line]}"
+    
+    @environment_field = IssueCustomField.find_or_initialize_by_name('Environment')
+    if @environment_field.new_record?
+      @environment_field.attributes = {:field_format => 'string', :default_value => 'production', :is_filter => true}
+      @environment_field.save(false)
+    end
+    
+    @version_field = IssueCustomField.find_or_initialize_by_name('Version')
+    if @version_field.new_record?
+      @version_field.attributes = {:field_format => 'string', :default_value => '', :is_filter => true}
+      @version_field.save(false)
+    end
   end
 end
